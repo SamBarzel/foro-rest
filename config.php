dc119f4d385d26ead50a9b971cd0bdc8a0156b39
SamBarzel <s20030169@cdhidalgo.tecnm.mx>
2023-10-15 11:18:16 -0600

    new

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+class Config{
+    const SERVER = "localhost";
+    const DB = "foro";
+    const USER = "root";
+    const PASS = "";
+    
+}
--- connection.php
+++ connection.php
@@ -1,11 +1,15 @@
 <?php
+
+require_once("config.php");
 function connection(){
 try{
-        $c = new PDO("mysql:host=localhost;dbname=foro", "root", "");
+        $c = new PDO("mysql:host=".Config::SERVER.";dbname=".Config::DB,
+        Config::USER,Config::PASS);
         $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $c->exec("use ".Config::DB);
         return $c;
 }catch(PDOException $e){
-    die("Error: " .$e->getMessage());
+    //die("Error: " .$e->getMessage());
+    exit($e->getMessage());
     }
-
 }
